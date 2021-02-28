b5f86073cc9379f7475a25c3edb7e02c0779e0b2
James Watson <james.watson-2@colorado.edu>
2021-02-27 17:01:41 -0700

    Universal Var

--- finch.cpp
+++ finch.cpp
@@ -1,5 +1,6 @@
 /********** INIT *********************************************************************************/
-#define INIT_NODE_NUM 64
+#define INIT_NODE_NUM       64
+#define DEFAULT_ALLOC_BYTES 64
 
 #include <iostream>
 #include <vector>
@@ -11,6 +12,31 @@ using std::endl;
 using std::vector;
 using std::shared_ptr;
 
+/********** VARIABLES ****************************************************************************/
+class Var{ public:
+
+void* data;
+
+Var(){  data = malloc( DEFAULT_ALLOC_BYTES );  }
+
+template<typename T>
+T get_as(){
+    return *((T*) data);
+}
+
+template<typename T>
+void set( T& data_ ){
+    *data = data_;
+}
+
+template<typename T>
+void set( const T& data_ ){
+    *data = data_;
+}
+
+};
+
+
 
 /********** NODE CLASSES *************************************************************************/
 
@@ -18,26 +44,28 @@ using std::shared_ptr;
 
 class Node{ public:
 
+void* data;
 
-std::shared_ptr<void> data;
-
-template<typename T>
-auto get(){
-    return *(std::static_pointer_cast<T>(data));
+auto get_ptr(){
+    return data;
 }
 
-// void set(const std::shared_ptr<void>& p) {
-//    data = p;
-// }
+template<typename T>
+T get(){
+    return *((T*) data);
+}
 
 template<typename T>
-void set(const T& p){
-   data = std::make_shared<void>( p );
+void set_ptr( T* data_ ){
+    data = (void*) data_;
+}
+
+template<typename T>
+void set( T& data_ ){
+    data = (void*) (&data_);
 }
 
 };
-
-
 
 
 /********** TEST/MAIN ****************************************************************************/
@@ -45,7 +73,10 @@ int main(){
 
 cout << "Test 1" << endl;
 Node /*----*/ foo{};
+int val = 1;
 
+foo.set_ptr( &val );
+cout << *(int*) foo.get_ptr() << ", " << foo.get<int>() << endl;
 
 
 
