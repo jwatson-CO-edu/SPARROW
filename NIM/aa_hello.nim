d174a577afede4131ec7657096ce3115386f79b3
James Watson <james.watson-2@colorado.edu>
2022-01-17 14:45:16 -0700

    installed new release and moved executable output, added compilation notes

--- NIM/aa_hello.nim
+++ NIM/aa_hello.nim
@@ -1,4 +1,6 @@
+## nim c -d:release --hints:off --run -o:exec/aa_hello -r aa_hello.nim ##
+
 # This is a comment
 echo "What's your name? "
-var name: string = readLine(stdin)
+var name: string = readLine( stdin )
 echo "Hi, ", name, "!"
--- NIM/ab_vars.nim
+++ NIM/ab_vars.nim
@@ -1,5 +1,12 @@
+## nim c -d:release --hints:off --run -o:exec/ab_vars -r ab_vars.nim ##
+
+# One var of one type
+var q: float
+
+# Many vars of one type
 var x, y: int # declares x and y to have the type ``int``
 
+# Many vars of many types
 var
   z, h: int
   # a comment can occur here too
