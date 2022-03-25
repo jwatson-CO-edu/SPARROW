073a5a7e52eccf5d71184a46cbe65327a9503c6b
James Watson <james.watson-2@colorado.edu>
2022-03-24 18:08:15 -0600

    reorg tuts and begin LISP eval

---
+++ Nim/10_Cpp_Eval/01_eval.nim
@@ -0,0 +1,8 @@
+#[ * Text:      [L]ISP [I]n [S]mall [P]ieces by Christian Quinnec 
+   * Chapter:   1
+   * Project:   SPARROW, [S]cheme [P]rogram [A]llowing [R]easonable [R]eckoning [O]f [W]ork
+   * Component: Micro-evaluator, written in C++ instead of Scheme ]#
+
+
+########## HELPER FUNCTIONS #######################################################################
+# FIXME: START HERE
