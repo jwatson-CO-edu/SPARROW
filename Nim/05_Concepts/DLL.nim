fb28cf5e646c9255f51c1c9b8f2a9a1499b2115e
James Watson <james.watson-2@colorado.edu>
2022-04-01 15:12:53 -0600

    DLL: Added a length member that changes with all list mutation operations, `get_by_index`, `get_node_by_index`, `get_value_by_index`, `insert_after_index`, `remove_by_index`, `choose_random_node`, `remove_random_node` - NEEDS TESTING

--- Nim/05_Concepts/DLL.nim
+++ Nim/05_Concepts/DLL.nim
@@ -5,6 +5,16 @@
     nim c -d:release --hints:off --run -o:exec/DLL -r DLL.nim
 ]# 
 
+
+########## INIT ####################################################################################
+
+import std/random # https://nim-lang.org/docs/random.html
+# Call randomize() once to initialize the default random number generator.
+# If this is not called, the same results will occur every time these
+# examples are run.
+randomize()
+
+
 ########## DLL TYPES ###############################################################################
 
 type
@@ -12,7 +22,7 @@ type
 
     DL_List[T] = object # Doubly-Linked List
         head, tail: Node[T]
-        # FIXME: ADD LENGTH
+        length: int
 
     Node[T] = ref object # DLL Node
         next, prev: Node[T]
@@ -33,6 +43,7 @@ proc newNode[T](data: T): Node[T] =
  
 proc prepend[T](l: var DL_List[T], n: Node[T]) =
     # Place a node at the head of the list
+    l.length += 1
     n.next = l.head
     if l.head != nil: l.head.prev = n
     l.head = n
@@ -40,6 +51,7 @@ proc prepend[T](l: var DL_List[T], n: Node[T]) =
  
 proc append[T](l: var DL_List[T], n: Node[T]) =
     # Place a node at the end of the list
+    l.length += 1
     n.next = nil
     n.prev = l.tail
     if l.tail != nil:
@@ -49,7 +61,8 @@ proc append[T](l: var DL_List[T], n: Node[T]) =
         l.head = n
  
 proc insertAfter[T](l: var DL_List[T], r, n: Node[T]) =
-    # Insert after the referenced node
+    # Insert after the `r`eferenced node
+    l.length += 1
     n.prev = r
     n.next = r.next
     n.next.prev = n
@@ -58,11 +71,16 @@ proc insertAfter[T](l: var DL_List[T], r, n: Node[T]) =
  
 proc remove[T](l: var DL_List[T], n: Node[T]) =
     # Remove the referenced node
+    if l.length > 0:  l.length -= 1
     if n == l.tail: l.tail = n.prev
     if n == l.head: l.head = n.next
     if n.next != nil: n.next.prev = n.prev
     if n.prev != nil: n.prev.next = n.next
  
+proc len[T](l: var DL_List[T]): int =
+    # Return the number of elements in the list
+    return l.length 
+
 proc `$`[T](l: var DL_List[T]): string =
     # String representation of the DLL
     result = ""
@@ -86,11 +104,48 @@ iterator traverseBackward[T](l: DL_List[T]): T =
         yield n.data
         n = n.prev
 
+proc get_node_by_index*[T]( l: DL_List[T], idx: int ): Node[T] =
+    # Get the element at position `idx`, if it exists, otherwise return `nil`, O(n)
+    if idx > -1 and idx < l.len():
+        for i, elem in traverseForward( l ):
+            if i == idx:
+                return elem
+    return nil
+
+proc get_value_by_index*[T]( l: DL_List[T], idx: int ): T =
+    # Get the element at position `idx`, if it exists, otherwise return `nil`, O(n)
+    if idx > -1 and idx < l.len():
+        for i, elem in traverseForward( l ):
+            if i == idx:
+                return elem.data
+    return nil
+
+proc insert_node_after_index*[T]( l: DL_List[T], idx: int, n: Node[T] ): bool =
+    # Find the node at `idx` and insert the given `n`ode after it, If successful, return true, else if DNE, return false
+    var r = get_node_by_index[T]( l, idx )
+    if r != nil:
+        insertAfter( l, r, n )
+        return true
+    return false
+
+proc remove_node_by_index*[T]( l: DL_List[T], idx: int ): bool =
+    # Find the node at `idx` and remove it, If successful, return true, else if DNE, return false
+    var n = get_node_by_index[T]( l, idx )
+    if n != nil:
+        remove( l, n )
+        return true
+    return false
+
+proc choose_random_node*[T]( l: DL_List[T] ): Node[T] =
+    # Return a random node in the list
+    return get_node_by_index( l, rand( l.len() ) )
+
+proc remove_random_node*[T]( l: DL_List[T] ): void =
+    # Return a random node in the list
+    remove_node_by_index( l, rand( l.len() ) )
+
+
 # FIXME:
-    # GET BY INDEX
-    # INSERT BY INDEX
-    # REMOVE BY INDEX
-    # CHOOSE RANDOM
     # REMOVE RANDOM
 
 ########## BASIC TESTS #############################################################################
