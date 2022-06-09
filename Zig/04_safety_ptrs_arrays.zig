72e9c733144ae596f5c4535d7c4e3825f6ce128b
James Watson <james.watson-2@colorado.edu>
2022-06-08 18:04:40 -0600

    begin enum study

--- Zig/04_safety_ptrs_arrays.zig
+++ Zig/04_safety_ptrs_arrays.zig
@@ -48,8 +48,84 @@ test "##### Unreachable Switch #####" {
 
 ////////// POINTERS ////////////////////////////////////////////////////////////////////////////////
 // https://ziglearn.org/chapter-1/#pointers
+// Normal pointers in Zig aren’t allowed to have 0 or null as a value. 
+// They follow the syntax *T, where T is the child type.
+// Referencing is done with &variable, and dereferencing is done with variable.
+
+fn increment(num: *u8) void { // Function takes an unsigned byte as an argument
+    num.* += 1;
+}
+
+test "##### Pointers #####" {
+    var x: u8 = 1;
+    increment(&x);
+    try expect(x == 2);
+}
+
+test "##### Zero Pointer Assignment #####" {
+    var x: u16 = 0;
+    var y: *u8 = @intToPtr(*u8, x); // FIXME: YOU SHOULD NOT BE ABLE TO ASSIGN POINTER TO ZERO
+    _ = y;
+}
 
 
+test "##### Const Pointers #####" {
+    // Zig also has const pointers, which cannot be used to modify the referenced data. 
+    // Referencing a const variable will yield a const pointer.
+    const x: u8 = 1;
+    var y = &x;
+    y.* += 1; // FIXME: YOU SHOULD NOT BE ABLE TO DO THIS
+}
+
+test "##### `usize`: The size of a pointer #####" {
+    try expect(@sizeOf(usize) == @sizeOf(*u8));
+    try expect(@sizeOf(isize) == @sizeOf(*u8));
+}
+
+///// Array Pointer /////
+// Sometimes you may have a pointer to an unknown amount of elements. 
+// [*]T is the solution for this, which works like *T but also supports indexing syntax, 
+// pointer arithmetic, and slicing. Unlike *T, it cannot point to a type which does not have a known size. 
+// *T coerces to [*]T
+
+///// Slices /////
+// Slices can be thought of as a pair of `[*]T` (the pointer to the data) and a `usize` (the element count). 
+// Their syntax is given as []T, with T being the child type. Slices are used heavily throughout Zig 
+// for when you need to operate on arbitrary amounts of data. Slices have the same attributes as pointers, 
+// meaning that there also exists const slices. For loops also operate over slices. 
+// String literals in Zig coerce to []const u8.
+
+// The syntax `x[n..m]` is used to create a slice from an array. This is called slicing, and creates 
+// a slice of the elements starting at `x[n]` and ending at `x[m - 1]`. This example uses a const 
+// slice as the values which the slice points to do not need to be modified.
+
+fn total(values: []const u8) usize {
+    var sum: usize = 0;
+    for (values) |v| sum += v;
+    return sum;
+}
+
+test "slices" {
+    const array = [_]u8{ 1, 2, 3, 4, 5 };
+    const slice = array[0..3];
+    try expect(total(slice) == 6);
+}
+
+
+test "slices 2" {
+    const array = [_]u8{ 1, 2, 3, 4, 5 };
+    // When these `n` and `m` values are both known at compile time, slicing will actually produce a 
+    // pointer to an array. This is not an issue as a pointer to an array i.e. `*[N]T` will coerce to a `[]T`.
+    const slice = array[0..3];
+    try expect(@TypeOf(slice) == *const [3]u8);
+}
+
+
+test "slices 3" {
+    var array = [_]u8{ 1, 2, 3, 4, 5 };
+    var slice = array[0..]; // The syntax `x[n..]` can also be used for when you want to slice to the end.
+    _ = slice;
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////// MAIN, UNUSED: ONLY HERE FOR COMPILATION PURPOSES ////////////////////////////////////////
