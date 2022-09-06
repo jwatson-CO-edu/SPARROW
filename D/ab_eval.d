f9bec95a540feb735c38eb1f3e1201b0cc4788a7
James Watson <james.watson-2@colorado.edu>
2022-09-06 11:28:34 -0600

    finish lexing, begin parsing, learning about conversions and exception handling

--- D/ab_eval.d
+++ D/ab_eval.d
@@ -8,9 +8,19 @@
    https://github.com/jwatson-CO-edu/FINCH/blob/main/Nim/10_Cpp_Eval/ab_eval.nim
    James Watson, 2022-09 */
 
+
+////////// INIT ////////////////////////////////////////////////////////////////////////////////////
+
 import std.string; // `string` type
 import std.stdio; //- `writeln`
 import std.conv; // - string conversions
+import std.uni; // -- `strip`
+
+///// Env Vars /////
+bool _DEBUG_VERBOSE = true;
+
+
+////////// ATOMS ///////////////////////////////////////////////////////////////////////////////////
 
 enum F_Error{ 
     OKAY    = "OKAY",    // No error code applicable
@@ -199,7 +209,7 @@ string str( Atom* item ){
 }
 
 
-////////// PARSING /////////////////////////////////////////////////////////////////////////////////
+////////// LEXING //////////////////////////////////////////////////////////////////////////////////
 string[string] RESERVED;
 
 void init_reserved(){
@@ -215,6 +225,7 @@ string find_reserved( string token ){
 }
 
 string[] tokenize( string expStr, dchar sepChar = ' ' ){
+    // Separate an expression string into tokens
     string[] tokens;
     dchar    c      = ' ';
     string   token  = "";
@@ -232,16 +243,47 @@ string[] tokenize( string expStr, dchar sepChar = ' ' ){
         // 2. Fetch character
         c = ch_i;
         // 3. Either add char to present token or create a new one
-
+        // A. Case Open Paren
+        if ( find_reserved( c.to!string() ) == "open_parn" ){  stow_char();  }  
+        // B. Case Close Paren
+        else if( find_reserved( c.to!string() ) == "clos_parn" ){  
+            if( token.length > 0 ){  stow_token();  }
+            stow_char();  
+        }
+        // C. Case separator
+        else if( (c == sepChar) && (token.length > 0) ){  stow_token();  }
+        // D. Case any other char
+        else{  cache_char();  }
     }
-
+    // N. Return the vector of tokens
     return tokens;
 }
     
 
+
+////////// PARSING /////////////////////////////////////////////////////////////////////////////////
+
+bool p_float_string( string inputStr ){
+    string slimStr = strip( inputStr );
+    try{
+        slimStr.to!double();
+        return true;
+    }catch( ConvException e ){
+        if(_DEBUG_VERBOSE){  writeln( "CONVERSION ERROR: Cannot convert \"", slimStr, "\"" );  }
+    }
+    return false;
+}
+
+// 2022-09-06: All tests PASS!
+
+// FIXME, START HERE: `p_empty_atom_string` {"", "\xE2\xA7\x84", "[/]" } // Replace `p_null_string`
+
+
 void main(){
+    // SPARROW Init //
     init_reserved();
     
+    // Atom Tests //
     Atom* mt = empty_atom(); 
     writeln( mt ); // Address of new atom
     writeln( p_empty( mt ) ); // true
@@ -255,6 +297,7 @@ void main(){
     writeln( p_cons( atm4 ) ); // true
     Atom* atm5 = make_error( F_Error.NAN, "Invalid number string" );
 
+    // String Representation Tests //
     writeln( str( mt ) ); // - ⧄
     writeln( str( atm1 ) ); // 2
     writeln( str( atm3 ) ); // ( 2, 3 )
@@ -265,7 +308,16 @@ void main(){
     writeln( str( atm4 ) ); // ( 3, ( 4, ( 5, ( 6, ⧄ ) ) ) )
     writeln( str( atm5 ) ); // ( ERROR: NAN, Invalid number string )
 
+    // Lexing Tests //
     writeln( find_reserved( "(" ) ); // - open_parn
     writeln( find_reserved( ")" ) ); // - clos_parn
     writeln( find_reserved( "foo" ) ); // <nothing>
+
+    string   exprString1 = "(foo a b)";
+    string[] exprTokens1 = tokenize( exprString1 );
+    writeln( exprTokens1 ); // ["(", "foo", "a", "b", ")"]
+
+    // Parsing Tests //
+    writeln( p_float_string( "42.5" ) );
+    writeln( p_float_string( "foo" ) );
 }
